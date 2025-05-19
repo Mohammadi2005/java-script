9c50d0189a891b81f0d055fb7eb4aa8b30508ab4
amir hossein <amohammadi161183@gmail.com>
2025-05-19 22:12:05 +0330

    BOM

---
+++ JS file/BOM.js
@@ -0,0 +1,19 @@
+// alert
+
+alert("wellcome my friend");
+
+
+// confirm
+
+let result1 = confirm("Are you a javaScript programer ?");
+let message1 = result1 ? "yes i am a javaScript programer" : "Noooooooooo";
+console.log(message1);
+
+
+// Prompt
+
+let result2 = prompt("what is your favorite programming language ? ");
+console.log(`${result2} is vere graet language`);
+
+
+
