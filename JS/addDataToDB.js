77ba18e39e7aafbff5d0ee66a3901c9c5daf3010
amir hossein <amohammadi161183@gmail.com>
2025-06-04 21:42:33 +0330

    add data to Indexed DB

---
+++ JS/addDataToDB.js
@@ -0,0 +1,72 @@
+const fnameInput = document.getElementById("fname");
+const lnameInput = document.getElementById("lname");
+const myForm = document.getElementById("myform");
+let db;
+
+window.addEventListener("load", () => {
+    let requ = window.indexedDB.open("NameDataBase", 1);
+
+    requ.onerror = () => {
+        console.log("Error");
+    }
+
+    requ.onsuccess = () => {
+        console.log("DataBase Opened Successfully");
+        db = requ.result;
+        console.log(db);
+    }
+
+
+    /////////////////////////////////////////////////
+    requ.onupgradeneeded = (e) => {
+        let db = e.target.result;
+
+        let objectStore = db.createObjectStore("NameDataBase", {
+            keyPath: "id",
+            autoIncrement: true
+        });
+
+        objectStore.createIndex("firstName", "FirstName", {
+            unique: false
+        });
+
+        objectStore.createIndex("lastName", "LastName", {
+            unique: false
+        });
+
+        console.log("Data Base Stupe Successfully...");
+    }
+})
+
+///////////////////////////////////////////////
+// add Data To DB
+
+const addData = (e) => {
+    e.preventDefault();
+    let newItem = {
+        firstName: fnameInput.value,
+        lastName: lnameInput.value
+    };
+
+    let transaction = db.transaction(["NameDataBase"], "readwrite");
+
+    let objectStore = transaction.objectStore("NameDataBase");
+
+    let request = objectStore.add(newItem);
+
+    request.onsuccess = () => {
+        fnameInput.value = "";
+        lnameInput.value = "";
+    }
+
+    transaction.oncomplete = () => {
+        console.log("transection Completed On Database");
+    }
+
+    transaction.onerror = () => {
+        console.log("Error transaction On Database");
+    }
+}
+
+myForm.addEventListener("submit", addData);
+
