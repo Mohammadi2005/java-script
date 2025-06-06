e2084bfecc31968449973bc59cbb9cdc4c0244b6
amir hossein <amohammadi161183@gmail.com>
2025-06-06 22:29:22 +0330

    SpreadOperator

---
+++ JS/SpreadOperator.js
@@ -0,0 +1,14 @@
+const name = ["ali", "amir", "mohammad", "saeed", "ahmad"]
+
+console.log(name)
+console.log(...name)
+
+var args = [2, 5, 6]
+
+function sum(a, b, c){
+    console.log(a + b + c)
+}
+
+sum(...args)
+
+
