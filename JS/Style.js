fb5969c40b9d27dcdf7641109406fb53ef910d6e
amir hossein <amohammadi161183@gmail.com>
2025-06-06 22:28:55 +0330

    style in js

---
+++ JS/Style.js
@@ -0,0 +1,6 @@
+var P = document.getElementById("myP");
+
+P.style.cssText = "padding : 20px; color : black; background-color : red; border-radius : 10px;";
+//P.setAttribute("style", "padding : 25px; color : white; background-color : blue; border-radius : 15px;")
+
+P.style.fontFamily = "cursive";
